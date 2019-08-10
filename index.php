b4f43953812fc352afa709a18b9620ffdade5cf5
zikrullah79 <zikrullahaliwainda@gmail.com>
2019-08-11 00:54:48 +0700

    database initialization

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "sub1-zik-database";
+    $user = "zikrullah79";
+    $pass = "Ryufar79";
+    $db = "sub1-zikdb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
