7b786535ddf37abecd1446bce9072b6cd6b543b1
zikrullah79 <zikrullahaliwainda@gmail.com>
2019-08-12 03:22:14 +0700

    test

--- analyze.php
+++ analyze.php
@@ -35,7 +35,7 @@ if(isset($_POST["img"])){
             "details": "",
             "language": "en",
         };
-        var sourceImageUrl = <?php echo $img ?>;
+        var sourceImageUrl = "<?php echo $img  ?>";
         document.querySelector("#sourceImage").src = sourceImageUrl;
  
         $.ajax({
