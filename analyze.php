9c8d4c12bbd1b96c5bcb85b8ce5f08b4bedfe599
zikrullah79 <zikrullahaliwainda@gmail.com>
2019-08-12 03:01:54 +0700

    test

--- analyze.php
+++ analyze.php
@@ -12,7 +12,7 @@ if(isset($_POST["img"])){
             "details": "",
             "language": "en",
         };
-        var sourceImageUrl = <?php echo "".$_POST["img"]."" ?>;
+        var sourceImageUrl = "https://subzikstorage.blob.core.windows.net/sub1pict/2017-retro-neptune-poseidon-trident-tombak-ukraina-yunani-kalung-trishul-simbol-kalung-untuk-pria-sweater-aksesoris.jpg"//<?php echo "".$_POST["img"]."" ?>;
         document.querySelector("#sourceImage").src = sourceImageUrl;
  
         $.ajax({
