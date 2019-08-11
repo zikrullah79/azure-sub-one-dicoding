ca467cdb0e45db00ede299064ef58b10ac9768b0
zikrullah79 <zikrullahaliwainda@gmail.com>
2019-08-12 02:32:17 +0700

    submission 2

--- index.php
+++ index.php
@@ -1,108 +1,90 @@
-<?php 
-require_once 'vendor/autoload.php';
-require_once "./random_string.php";
-
-use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
-use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
-use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
-use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
-
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=subzikstorage;AccountKey=ekPoRJ4XJ9v/47j+i0IIiRNDHINTzqukS1tNw8oibcbk/Rtr5r8YK3WyyaE+M2CqcwXAUqF19j4o6SbGc8wIhw==";
-
-$containerName = "sub1pict";
-// Create blob client.
-$blobClient = BlobRestProxy::createBlobService($connectionString);
-if (isset($_POST["upload"]) && isset($_FILES["browse"])) {
-        $fileToUpload = strtolower($_FILES["browse"]["name"]);
-          if ($fileToUpload != ''){
-            try {
-                // Create container.
-                // $blobClient->createContainer($containerName, $createContainerOptions);
-        
-                // Getting local file so that we can upload it to Azure
-                $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
-                fclose($myfile);
-                
-                $content = fopen($fileToUpload, "r");
-        
-                $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-                ?><script>alert("Succesfully add Image")</script><?php
-            }
-            catch(ServiceException $e){
-                // Handle exception based on error codes and messages.
-                // Error codes and messages are here:
-                // http://msdn.microsoft.com/library/azure/dd179439.aspx
-                $code = $e->getCode();
-                $error_message = $e->getMessage();
-                echo $code.": ".$error_message."<br />";
-            }
-            catch(InvalidArgumentTypeException $e){
-                // Handle exception based on error codes and messages.
-                // Error codes and messages are here:
-                // http://msdn.microsoft.com/library/azure/dd179439.aspx
-                $code = $e->getCode();
-                $error_message = $e->getMessage();
-                echo $code.": ".$error_message."<br />";
-            }
-          }else {
-            ?>
-                <script>
-                    alert("Check Your Form Again")
-                </script>
-            <?php
-        }
-    }
-    $listBlobsOptions = new ListBlobsOptions();
-    $listBlobsOptions->setPrefix("");
-    $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-?>
 <html>
  <head>
  <Title>Registration Form</Title>
-    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
  <style type="text/css">
+ 	body { background-color: #fff; border-top: solid 10px #000;
+ 	    color: #333; font-size: .85em; margin: 20; padding: 20;
+ 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+ 	}
+ 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+ 	h1 { font-size: 2em; }
+ 	h2 { font-size: 1.75em; }
+ 	h3 { font-size: 1.2em; }
+ 	table { margin-top: 0.75em; }
+ 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+ 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
  </style>
  </head>
  <body>
- <div class="container">
-    <h1>Add Here !</h1>
-    <p>Add Your Picture, Click <strong>upload</strong> to add.</p>
-    <form class="form-inline" method="POST" action="phpQS.php" enctype="multipart/form-data" >
-        <div class="form-group">
-            <div class="col-sm-10">
-                <input type="file" name="browse" accept="image/x-png,image/jpeg" />
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-10">
-                <input class="btn btn-primary" type="submit" name="upload" />
-            </div>
-        </div>
-    </form>
-    <form  method="POST" action="analyze.php">
-        <table class="table table-condensed">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php 
-                do{
-                    foreach ($result->getBlobs() as $blob)
-                    {
-                        echo "<tr><td>".$blob->getName()."</td><td><input name='submit' type ='submit' value='analyze'></input><input type='hidden' name='img' value = '".$blob->getUrl()."'/></td></tr>";
-                    }
-                
-                    $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-                } while($result->getContinuationToken());
-            ?>
-            </tbody>
-        </table>
-    </form>
- </div>
+ <h1>Register here !</h1>
+ <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+ <form method="post" action="index.php" enctype="multipart/form-data" >
+       Name  <input type="text" name="name" id="name"/></br></br>
+       Email <input type="text" name="email" id="email"/></br></br>
+       Job <input type="text" name="job" id="job"/></br></br>
+       <input type="submit" name="submit" value="Submit" />
+       <input type="submit" name="load_data" value="Load Data" />
+ </form>
+ <?php
+    $host = "sub1-zik-database.database.windows.net";
+    $user = "zikrullah79";
+    $pass = "Ryufar79";
+    $db = "sub1-zikdb";
+
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
+
+    if (isset($_POST['submit'])) {
+        try {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $job = $_POST['job'];
+            $date = date("Y-m-d");
+            // Insert data
+            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
+                        VALUES (?,?,?,?)";
+            $stmt = $conn->prepare($sql_insert);
+            $stmt->bindValue(1, $name);
+            $stmt->bindValue(2, $email);
+            $stmt->bindValue(3, $job);
+            $stmt->bindValue(4, $date);
+            $stmt->execute();
+            echo "<h3>Your're registered!</h3>";
+        } catch(Exception $e) {
+            echo "Failed: " . $e;
+        }
+
+       
+    } else if (isset($_POST['load_data'])) {
+        try {
+            $sql_select = "SELECT * FROM Registration";
+            $stmt = $conn->query($sql_select);
+            $registrants = $stmt->fetchAll(); 
+            if(count($registrants) > 0) {
+                echo "<h2>People who are registered:</h2>";
+                echo "<table>";
+                echo "<tr><th>Name</th>";
+                echo "<th>Email</th>";
+                echo "<th>Job</th>";
+                echo "<th>Date</th></tr>";
+                foreach($registrants as $registrant) {
+                    echo "<tr><td>".$registrant['name']."</td>";
+                    echo "<td>".$registrant['email']."</td>";
+                    echo "<td>".$registrant['job']."</td>";
+                    echo "<td>".$registrant['date']."</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "<h3>No one is currently registered.</h3>";
+            }
+        } catch(Exception $e) {
+            echo "Failed: " . $e;
+        }
+    }
+ ?>
  </body>
-</html>
+ </html>
---
+++ sub2.php
@@ -0,0 +1,108 @@
+<?php 
+require_once 'vendor/autoload.php';
+require_once "./random_string.php";
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=subzikstorage;AccountKey=ekPoRJ4XJ9v/47j+i0IIiRNDHINTzqukS1tNw8oibcbk/Rtr5r8YK3WyyaE+M2CqcwXAUqF19j4o6SbGc8wIhw==";
+
+$containerName = "sub1pict";
+// Create blob client.
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+if (isset($_POST["upload"]) && isset($_FILES["browse"])) {
+        $fileToUpload = strtolower($_FILES["browse"]["name"]);
+          if ($fileToUpload != ''){
+            try {
+                // Create container.
+                // $blobClient->createContainer($containerName, $createContainerOptions);
+        
+                // Getting local file so that we can upload it to Azure
+                $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
+                fclose($myfile);
+                
+                $content = fopen($fileToUpload, "r");
+        
+                $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+                ?><script>alert("Succesfully add Image")</script><?php
+            }
+            catch(ServiceException $e){
+                // Handle exception based on error codes and messages.
+                // Error codes and messages are here:
+                // http://msdn.microsoft.com/library/azure/dd179439.aspx
+                $code = $e->getCode();
+                $error_message = $e->getMessage();
+                echo $code.": ".$error_message."<br />";
+            }
+            catch(InvalidArgumentTypeException $e){
+                // Handle exception based on error codes and messages.
+                // Error codes and messages are here:
+                // http://msdn.microsoft.com/library/azure/dd179439.aspx
+                $code = $e->getCode();
+                $error_message = $e->getMessage();
+                echo $code.": ".$error_message."<br />";
+            }
+          }else {
+            ?>
+                <script>
+                    alert("Check Your Form Again")
+                </script>
+            <?php
+        }
+    }
+    $listBlobsOptions = new ListBlobsOptions();
+    $listBlobsOptions->setPrefix("");
+    $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+?>
+<html>
+ <head>
+ <Title>Registration Form</Title>
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+ <style type="text/css">
+ </style>
+ </head>
+ <body>
+ <div class="container">
+    <h1>Add Here !</h1>
+    <p>Add Your Picture, Click <strong>upload</strong> to add.</p>
+    <form class="form-inline" method="POST" action="phpQS.php" enctype="multipart/form-data" >
+        <div class="form-group">
+            <div class="col-sm-10">
+                <input type="file" name="browse" accept="image/x-png,image/jpeg" />
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-10">
+                <input class="btn btn-primary" type="submit" name="upload" />
+            </div>
+        </div>
+    </form>
+    <form  method="POST" action="analyze.php">
+        <table class="table table-condensed">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php 
+                do{
+                    foreach ($result->getBlobs() as $blob)
+                    {
+                        echo "<tr><td>".$blob->getName()."</td><td><input name='submit' type ='submit' value='analyze'></input><input type='hidden' name='img' value = '".$blob->getUrl()."'/></td></tr>";
+                    }
+                
+                    $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+                } while($result->getContinuationToken());
+            ?>
+            </tbody>
+        </table>
+    </form>
+ </div>
+ </body>
+</html>
